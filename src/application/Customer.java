8fd7a827f8864ee4788677345abca455b7802ca1
ddthang94 <ddthang21194@gmail.com>
2022-02-17 22:31:31 +0200

    UI updated

--- src/application/Customer.java
+++ src/application/Customer.java
@@ -1,5 +1,83 @@
 package application;
 
 public class Customer {
+	private String fname;
+	private String lname;
+	private String username;
+	private String password;
+	private String phoneNum;
+	private String email;
+	private String doBirth;
+	private String gender;
+	private float balance;
+	
+	/* Constructors */
+	public Customer(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
 
+	public Customer(String fname, String lname, String username, String phoneNum, String email, String doBirth, String gender, float balance) {
+		this.fname = fname;
+		this.lname = lname;
+		this.username = username;
+		this.phoneNum = phoneNum;
+		this.email = email;
+		this.doBirth = doBirth;
+		this.gender = gender;
+		this.balance = balance;
+	}
+
+	public Customer(String fname, String lname, String username, String password, String phoneNum, String email, String doBirth, String gender) {
+		this.fname = fname;
+		this.lname = lname;
+		this.username = username;
+		this.password = password;
+		this.phoneNum = phoneNum;
+		this.email = email;
+		this.doBirth = doBirth;
+		this.gender = gender;
+		this.balance = 0f;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getDoBirth() {
+		return doBirth;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public float getBalance() {
+		return balance;
+	}
+
+	public void setBalance(float balance) {
+		this.balance = balance;
+	}
+	
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -9,23 +9,32 @@ import javafx.scene.image.Image;
 
 
 public class Main extends Application {
+	
+	static Stage pstage;
+
 	@Override
 	public void start(Stage primaryStage) {
+		pstage = primaryStage;
+		changeScene("SignIn.fxml");
+	}
+	
+	// changeScene() function is used to switch between Scenes(Page)
+	public void changeScene(String gui) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("SignIn.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource(gui)); // loading the FXML file
 			Scene scene = new Scene(root);
-			Image icon = new Image("icon.png");
+			Image icon = new Image("icon.png"); // create object icon
 			
-			primaryStage.setTitle("Bank Management System");
-			primaryStage.getIcons().add(icon);
-			primaryStage.setScene(scene);
-			primaryStage.setResizable(false);
-			primaryStage.show();
+			pstage.setTitle("Bank Management System"); // set the software title
+			pstage.getIcons().add(icon); // add icon on title
+			pstage.setScene(scene); //scene=content inside window
+			pstage.setResizable(false); //window cannot resize
+			pstage.show(); // showing window on screen
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+		
 	public static void main(String[] args) {
 		launch(args);
 	}
